40786eedea57d78c93b28a1ce9c2fea4785a0141
Jackoday <jacksonday@hotmail.co.nz>
2024-02-01 18:46:10 +1300

    Basic level generation complete

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,49 @@
 extends Node2D
 
+@onready var platform_parent = $PlatformParent
 
-# Called when the node enters the scene tree for the first time.
+var camera_scene = preload("res://scenes/game_camera.tscn")
+var platform_scene = preload("res://scenes/platform.tscn")
+
+var camera = null
+
+# Level gen variables
+var start_platform_y
+var y_distance_between_platforms = 100
+var level_size = 50
+
 func _ready():
-	pass # Replace with function body.
+	camera = camera_scene.instantiate()
+	camera.setup_camera($Player)
+	add_child(camera)
+	
+	#Generate the ground
+	var viewport_size = get_viewport_rect().size
+	var y_position = viewport_size.y - 60
+	var platform_width = platform_scene.instantiate().getWidth() + 2 #gap between
+	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+	
+	for i in range(ground_layer_platform_count):
+		create_platform(Vector2(i * platform_width, y_position))
+		
+	# Generate the rest of the level
+	start_platform_y = viewport_size.y - (y_distance_between_platforms * 2)
+	
+	for i in range(level_size):
+		var location: Vector2
+		location.x = randf_range(0, viewport_size.x - platform_width)
+		location.y = start_platform_y - (y_distance_between_platforms * i)
+		
+		create_platform(location)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func create_platform(location: Vector2):
+	var platform = platform_scene.instantiate()
+	platform.global_position = location
+	platform_parent.add_child(platform)
+	return platform
---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+class_name Player
+
+@onready var ap = $AnimationPlayer
+
+var speed = 300.0
+var gravity = 15.0
+var terminal_velocity = 1000
+var jump_velocity = -800
+var viewport_size
+
+func _ready():
+	viewport_size = get_viewport_rect().size
+
+func _process(delta):
+	if velocity.y > 0 and ap.current_animation != "fall":
+		ap.play("fall")
+	elif velocity.y < 0 and ap.current_animation != "jump":
+		ap.play("jump")
+
+func _physics_process(delta):
+	velocity.y += gravity
+	if velocity.y > terminal_velocity:
+		velocity.y = terminal_velocity
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	move_and_slide()
+	
+	var margin = 20
+	if global_position.x > viewport_size.x + margin:
+		global_position.x = -margin
+	elif global_position.x < -margin:
+		global_position.x = viewport_size.x + margin
+
+func jump():
+	velocity.y = jump_velocity
